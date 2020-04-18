0251485fa6c8b12672bb6b77337d253e97878651
Владимир Шуваев <shuvaev32167@gmail.com>
2020-04-18 21:01:58 +0300

    Анализ проблемы декарта

--- app/src/main/java/ru/crazypeppers/problemsassistant/fragment/tabled/AnalyzeProblemSummaryFragment.kt
+++ app/src/main/java/ru/crazypeppers/problemsassistant/fragment/tabled/AnalyzeProblemSummaryFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_analyze_problem_summery.*
@@ -13,8 +14,11 @@ import ru.crazypeppers.problemsassistant.activity.MainActivity
 import ru.crazypeppers.problemsassistant.data.DATE_FORMAT
 import ru.crazypeppers.problemsassistant.data.NOT_POSITION
 import ru.crazypeppers.problemsassistant.data.PROBLEM_POSITION_TEXT
+import ru.crazypeppers.problemsassistant.data.dto.DescartesSquaredCard
 import ru.crazypeppers.problemsassistant.data.dto.LinearCard
 import ru.crazypeppers.problemsassistant.data.enumiration.CardType
+import ru.crazypeppers.problemsassistant.data.enumiration.ProblemType
+import ru.crazypeppers.problemsassistant.fromHtml
 import ru.crazypeppers.problemsassistant.toStringRound
 
 /**
@@ -39,43 +43,105 @@ AnalyzeProblemSummaryFragment : Fragment() {
         if (arg != null) {
             val positionProblem = arg.getInt(PROBLEM_POSITION_TEXT, NOT_POSITION)
             val problem = (activity.application as DataApplication).data[positionProblem]
-            val avgPoint = problem.calculateScoreProblem()
+            if (problem.type == ProblemType.LINE) {
+                linearProblemLayout.visibility = VISIBLE
+                descartesSquaredProblemLayout.visibility = GONE
 
-            summeryInformation.text = String.format(
-                getString(R.string.avgPointsLabel), problem.name,
-                if (avgPoint.isNaN()) "0.00" else avgPoint.toStringRound(2)
-            )
+                val cards = problem.cards.filterIsInstance<LinearCard>()
 
-            countMotivation.text = String.format(
-                getString(R.string.countMotivationLabel),
-                problem.cards.filter { it.type == CardType.LINEAR_ADVANTAGE }.count()
-            )
+                val avgPoint = problem.calculateScoreProblem()
 
-            countAnchor.text = String.format(
-                getString(R.string.countAnchorLabel),
-                problem.cards.filter { it.type == CardType.LINEAR_DISADVANTAGE }.count()
-            )
-
-            summeryCountCard.text = String.format(
-                getString(R.string.summaryCountCardLabel), problem.cards.count()
-            )
-
-            val assessments = problem.cards.filterIsInstance<LinearCard>().flatMap { it.points }
-                .sortedBy { it.cdate }
-
-            if (assessments.isNotEmpty()) {
-                dateOfFirstProblemAssessment.text = String.format(
-                    getString(R.string.dateOfFirstProblemAssessmentLabel),
-                    DATE_FORMAT.format(assessments.first().cdate.time)
+                summeryInformation.text = String.format(
+                    getString(R.string.avgPointsLabel), problem.name,
+                    if (avgPoint.isNaN()) "0.00" else avgPoint.toStringRound(2)
                 )
 
-                dateOfLastProblemAssessment.text = String.format(
-                    getString(R.string.dateOfLastProblemAssessmentLabel),
-                    DATE_FORMAT.format(assessments.last().cdate.time)
+                countMotivation.text = String.format(
+                    getString(R.string.countMotivationLabel),
+                    cards.filter { it.type == CardType.LINEAR_ADVANTAGE }.count()
                 )
-            } else {
-                dateOfFirstProblemAssessment.visibility = GONE
-                dateOfLastProblemAssessment.visibility = GONE
+
+                countAnchor.text = String.format(
+                    getString(R.string.countAnchorLabel),
+                    cards.filter { it.type == CardType.LINEAR_DISADVANTAGE }.count()
+                )
+
+                summeryCountCard.text = String.format(
+                    getString(R.string.summaryCountCardLabel), cards.count()
+                )
+
+                val assessments = cards.flatMap { it.points }
+                    .sortedBy { it.cdate }
+
+                if (assessments.isNotEmpty()) {
+                    dateOfFirstProblemAssessment.text = String.format(
+                        getString(R.string.dateOfFirstProblemAssessmentLabel),
+                        DATE_FORMAT.format(assessments.first().cdate.time)
+                    )
+
+                    dateOfLastProblemAssessment.text = String.format(
+                        getString(R.string.dateOfLastProblemAssessmentLabel),
+                        DATE_FORMAT.format(assessments.last().cdate.time)
+                    )
+                } else {
+                    dateOfFirstProblemAssessment.visibility = GONE
+                    dateOfLastProblemAssessment.visibility = GONE
+                }
+            } else if (problem.type == ProblemType.DESCARTES_SQUARED) {
+                linearProblemLayout.visibility = GONE
+                descartesSquaredProblemLayout.visibility = VISIBLE
+
+                val cards = problem.cards.filterIsInstance<DescartesSquaredCard>()
+                    .sortedBy { it.createCardDate }
+
+                descartesSquaredCountIGroup.text = fromHtml(
+                    getString(R.string.descartesSquaredGroupCardCountLabel,
+                        getString(R.string.descartesSquaredIQuarterFotGetString),
+                        cards.count { it.type == CardType.SQUARE_DO_HAPPEN }
+                    )
+                )
+
+                descartesSquaredCountIIGroup.text = fromHtml(
+                    getString(R.string.descartesSquaredGroupCardCountLabel,
+                        getString(R.string.descartesSquaredIIQuarterFotGetString),
+                        cards.count { it.type == CardType.SQUARE_NOT_DO_HAPPEN }
+                    )
+                )
+
+                descartesSquaredCountIIIGroup.text = fromHtml(
+                    getString(R.string.descartesSquaredGroupCardCountLabel,
+                        getString(R.string.descartesSquaredIIIQuarterFotGetString),
+                        cards.count { it.type == CardType.SQUARE_DO_NOT_HAPPEN }
+                    )
+                )
+
+                descartesSquaredCountIVGroup.text = fromHtml(
+                    getString(R.string.descartesSquaredGroupCardCountLabel,
+                        getString(R.string.descartesSquaredIVQuarterFotGetString),
+                        cards.count { it.type == CardType.SQUARE_NOT_DO_NOT_HAPPEN }
+                    )
+                )
+
+                descartesSquaredSummeryCountCard.text =
+                    getString(R.string.summaryCountCardLabel, cards.count())
+
+                if (cards.isNotEmpty()) {
+                    descartesSquaredDateOfFistCardCreate.visibility = VISIBLE
+                    descartesSquaredDateOfLastCardCreate.visibility = VISIBLE
+
+                    descartesSquaredDateOfFistCardCreate.text = getString(
+                        R.string.descartesSquaredDateOfFistCardCreateLabel,
+                        DATE_FORMAT.format(cards.first().createCardDate.time)
+                    )
+
+                    descartesSquaredDateOfLastCardCreate.text = getString(
+                        R.string.descartesSquaredDateOfLastCardCreateLabel,
+                        DATE_FORMAT.format(cards.last().createCardDate.time)
+                    )
+                } else {
+                    descartesSquaredDateOfFistCardCreate.visibility = GONE
+                    descartesSquaredDateOfLastCardCreate.visibility = GONE
+                }
             }
         }
     }
